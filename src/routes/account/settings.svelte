6f90b981976b582156fbddd745646e2347c0a43b
Chris K <88936353+chris-pica@users.noreply.github.com>
2022-01-15 19:02:15 +0800

    Add My Account page

---
+++ src/routes/account/settings.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Account from '$lib/components/auth/Account.svelte';
+</script>
+
+<svelte:head>
+	<title>Radix List | My Account</title>
+	<meta property="og:title" content="Radix List | My Account" />
+	<meta name="twitter:title" content="Radix List | My Account" />
+</svelte:head>
+
+<Account />
