1c88f3e737a3b9b8890f1a62da879c0487be912f
Simon Wikstrand <simon@wikstrand.dev>
2022-04-01 14:07:42 +0200

    reset project, latest sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,10 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({})],
+	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
